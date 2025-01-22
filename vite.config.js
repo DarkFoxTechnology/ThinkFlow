31814fb2c9c9d3ab27b417cce052aebaac2aa2aa
godmid <494752893@qq.com>
2025-01-22 09:20:59 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -23,16 +23,29 @@ export default defineConfig({
           'mindmap': ['konva', 'marked'],
           'collaboration': ['yjs', 'y-webrtc', 'y-indexeddb']
         }
+      },
+      external: ['vue'],
+      input: {
+        main: resolve(__dirname, 'index.html')
       }
     },
-    chunkSizeWarningLimit: 1000
+    chunkSizeWarningLimit: 1000,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src')
+      '@': resolve(__dirname, './src'),
+      'vue': 'vue/dist/vue.esm-bundler.js'
     }
   },
   optimizeDeps: {
-    include: ['vue', 'pinia', 'yjs', 'socket.io-client']
+    include: ['vue', 'pinia', 'yjs', 'socket.io-client'],
+    exclude: []
   }
 })
