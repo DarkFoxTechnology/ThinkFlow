95e3386f462973357814b08d2abad862c6795e9d
godmid <494752893@qq.com>
2025-01-22 09:17:23 +0800

    添加404页面，方便排查问题

--- vite.config.js
+++ vite.config.js
@@ -29,8 +29,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
-      'vue': 'vue/dist/vue.esm-bundler.js'
+      '@': resolve(__dirname, './src')
     }
   },
   optimizeDeps: {
